20852bd82ded1608a5d8115c2e248b1b36ecca05
ibrag <ibragimov-amir@list.ru>
2023-04-28 04:40:55 +0500

    Added DbSet in DataContext.cs

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+        
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
