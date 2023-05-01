e0f8bf68f9406db8e4999174f916ace3679c68de
ibrag <ibragimov-amir@list.ru>
2023-05-01 13:27:57 +0500

    Added BaseApiController.cs based on MediatR

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly TokenService _tokenService;
+        
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+        
+        
+    }
+}
